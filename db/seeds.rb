1b431a089c03461531f186758b768ff4d63d55c4
Alice Laurin <laurin.alice@gmail.com>
2023-11-14 17:04:33 +0100

    ex 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+Article.destroy_all
+
+100.times do
+  article = Article.new(
+    title:Faker::Movie.title,
+    content:Faker::Lorem.sentence(word_count: 100)
+  )
+  article.save!
+end
